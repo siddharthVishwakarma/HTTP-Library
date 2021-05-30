7c696bd199904a57d154cc80cfaab80281b9a520
Siddharth Vishwakarma <sid.sv14@gmail.com>
2021-05-31 00:12:41 +0530

    post request added

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const HTTP = new HTTPrequest;
 
-HTTP.get('https://jsonplaceholder.typicode.com/users')
+// Making a GET request
+// HTTP.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+const data = {
+    Name: 'John Doe',
+    Email: 'johndoe@gmail.com',
+    Username: 'johndoe'
+}
+
+// Making a POST request   
+HTTP.post('https://jsonplaceholder.typicode.com/users', data)
     .then(data => console.log(data))
     .catch(err => console.log(err));
-
-             
--- http.js
+++ http.js
@@ -9,10 +9,26 @@
 
 class HTTPrequest {
 
-    //Make a get request
+    //Make a http get request
     get(url){
         return new Promise((resolve, reject) => {
             fetch(url)
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .then(err => reject(err));
+        })
+    }
+
+    //Make a http post request
+    post(url, data){
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type' : 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
             .then(res => res.json())
             .then(data => resolve(data))
             .then(err => reject(err));
